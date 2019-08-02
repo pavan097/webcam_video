8342c4966af74bac009ac385b62465967485c49a
pavan097 <pavan970535@gmail.com>
2019-08-02 23:15:34 +0530

    add todolist

--- videoCap.py
+++ videoCap.py
@@ -6,7 +6,7 @@ import numpy as np
 c = cv2.VideoCapture(0) # 0 for the inbuilt camera i.e webcam
 while True:
     r, frame = c.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert to gray
     cv2.imshow('frame :',gray)
     if cv2.waitKey(1) & 0XFF == ord('q'):
         break
