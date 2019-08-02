e13fb9642efe8a17b81322cb811fefa48a6897eb
pa1 <pavan970535@gmail.com>
2019-08-02 22:43:41 +0530

    Add files via upload
    record the video through webcam

---
+++ videoCap.py
@@ -0,0 +1,15 @@
+
+import cv2
+import numpy as np
+
+c = cv2.VideoCapture(0) # 0 for the inbuilt camera i.e webcam
+while True:
+    r, frame = c.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame :',gray)
+    if cv2.waitKey(1) & 0XFF == ord('q'):
+        break
+
+cv2.waitKey(1)
+c.release()
+cv2.destroyAllWindows()
