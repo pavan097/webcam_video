b8d6106541f94351da9cbeeb5d0ff6544b5175dc
pa1 <pavan970535@gmail.com>
2019-08-06 22:50:26 +0530

    Update videoCap.py
    single frame

--- videoCap.py
+++ videoCap.py
@@ -4,12 +4,18 @@ import cv2
 import numpy as np
 
 c = cv2.VideoCapture(0) # 0 for the inbuilt camera i.e webcam
-while True:
-    r, frame = c.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert to gray
-    cv2.imshow('frame :',gray)
-    if cv2.waitKey(1) & 0XFF == ord('q'):
-        break
+# series of snapshots (for video)
+# while True:
+#     r, frame = c.read()
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert to gray
+#     cv2.imshow('frame :',gray)
+#     if cv2.waitKey(1) & 0XFF == ord('q'):
+#         break             
+
+# single snapshot
+_, frame = c.read()
+cv2.imshow('frame :',frame)
+
 
 cv2.waitKey(1)
 c.release()
